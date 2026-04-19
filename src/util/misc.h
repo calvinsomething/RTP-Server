f6a4bc147f3a2816628d476b1b345afb2c0cdf6b
Calvin Storoschuk <calvinsomething@gmail.com>
2026-04-19 15:33:25 -0400

    add parsing utils

--- src/util/misc.h
+++ src/util/misc.h
@@ -1,8 +1,25 @@
 #pragma once
 
+#include <charconv>
 #include <cstddef>
+#include <string>
+#include <type_traits>
 
 template <typename T, size_t N> constexpr size_t array_size(T (&)[N])
 {
     return N;
+}
+
+template <typename T, typename std::enable_if<std::is_integral<T>::value>> std::string parse_int(T i)
+{
+    std::string buffer(20, 0);
+    std::to_chars(buffer.begin(), buffer.end(), i);
+    return buffer;
+}
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value>> std::string parse_float(T f)
+{
+    std::string buffer(48, 0);
+    std::to_chars(buffer.begin(), buffer.end(), f);
+    return buffer;
 }
